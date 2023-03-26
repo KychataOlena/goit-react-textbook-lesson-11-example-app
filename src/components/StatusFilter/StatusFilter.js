927a381600b2c3cd59998aec62bd39bcb1c14fe9
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2023-03-26 23:45:44 +0300

    store

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,16 @@
+import { Button } from 'components/Button/Button';
+import css from './StatusFilter.module.css';
+import { useSelector } from 'react-redux';
+import { statusFilters } from '../../redux/constants';
+import { getStatusFilter } from '../../redux/selectors';
+export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+  return (
+    <div>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
+    </div>
+  );
+};
---
+++ src/services.js
@@ -0,0 +1,2 @@
+export const API_KEY = '37c19565ff9fd1caddc6961e74d76e1e';
+export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500/';
